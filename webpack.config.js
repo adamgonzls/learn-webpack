7eab55da43e8921e904a02d421fc1ead7f48f561
Adam Gonzales <agonzales@tethertools.com>
2021-12-02 16:24:22 -0700

    Add loaders to handle css - installed style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
