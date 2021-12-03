66c0a3648a936a46f5cef474a1d61095c9c8a4ec
Adam Gonzales <agonzales@tethertools.com>
2021-12-02 19:22:50 -0700

    Add Sass loader, override bootstrap colors - change main.css to main.scss - update webpack.config to use scss

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 }
